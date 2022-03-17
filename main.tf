5c49238492311c9b70461760a9eb5be2ea94622f
Miles <miles.harrison@hashicorp.com>
2022-03-17 05:54:27 -0500

    adding dns support

--- main.tf
+++ main.tf
@@ -140,8 +140,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 3.0"
 
-  name = local.name
-  cidr = "10.99.0.0/18"
+  name                 = local.name
+  cidr                 = "10.99.0.0/18"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
 
   azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
   public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
