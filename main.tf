15f033b1a9e291fecfedaae6c065a5b9b3ab5705
Miles <miles.harrison@hashicorp.com>
2022-03-17 03:37:37 -0500

    Updating SG with HVN cidr

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ module "security_group" {
       to_port     = 5432
       protocol    = "tcp"
       description = "PostgreSQL access from within VPC"
-      cidr_blocks = module.vpc.vpc_cidr_block
+      cidr_blocks = [module.vpc.vpc_cidr_block, data.hcp_hvn.cidr_block]
     },
   ]
 
