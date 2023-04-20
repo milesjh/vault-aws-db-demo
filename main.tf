a00789a6403494f4e93af60bd666f4dc819738d5
Miles <miles.harrison@hashicorp.com>
2023-04-19 21:15:54 -0500

    Code reset

--- main.tf
+++ main.tf
@@ -13,20 +13,20 @@ terraform {
     }
   }
 
-  required_version = ">= 0.13.1"
+  required_version = ">= 1.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 4"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = ">= 3.0"
+      version = "~> 3"
     }
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.24.0"
+      version = "~> 0.56"
     }
   }
 }
@@ -158,91 +158,91 @@ module "vpc" {
   tags = local.tags
 }
 
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 4.0"
+# module "security_group" {
+#   source  = "terraform-aws-modules/security-group/aws"
+#   version = "~> 4.0"
 
-  name        = local.name
-  description = "Complete PostgreSQL example security group"
-  vpc_id      = module.vpc.vpc_id
+#   name        = local.name
+#   description = "Complete PostgreSQL example security group"
+#   vpc_id      = module.vpc.vpc_id
 
-  # ingress
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 5432
-      to_port     = 5432
-      protocol    = "tcp"
-      description = "PostgreSQL access from within VPC"
-      cidr_blocks = join(",", flatten([module.vpc.vpc_cidr_block, hcp_hvn.demo.cidr_block]))
-    },
-  ]
+#   # ingress
+#   ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 5432
+#       to_port     = 5432
+#       protocol    = "tcp"
+#       description = "PostgreSQL access from within VPC"
+#       cidr_blocks = join(",", flatten([module.vpc.vpc_cidr_block, hcp_hvn.demo.cidr_block]))
+#     },
+#   ]
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-################################################################################
-# RDS Module
-################################################################################
+# ################################################################################
+# # RDS Module
+# ################################################################################
 
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
+# module "db" {
+#   source = "terraform-aws-modules/rds/aws"
 
-  identifier = local.name
+#   identifier = local.name
 
-  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-  engine               = "postgres"
-  engine_version       = "14.1"
-  family               = "postgres14" # DB parameter group
-  major_engine_version = "14"         # DB option group
-  instance_class       = "db.t4g.micro"
+#   # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
+#   engine               = "postgres"
+#   engine_version       = "14.1"
+#   family               = "postgres14" # DB parameter group
+#   major_engine_version = "14"         # DB option group
+#   instance_class       = "db.t4g.micro"
 
-  allocated_storage     = 20
-  max_allocated_storage = 100
+#   allocated_storage     = 20
+#   max_allocated_storage = 100
 
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  db_name             = "demopostgresqldb"
-  username            = "vaultuser"
-  port                = 5432
-  publicly_accessible = false
+#   # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+#   # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+#   # user cannot be used as it is a reserved word used by the engine"
+#   db_name             = "demopostgresqldb"
+#   username            = "vaultuser"
+#   port                = 5432
+#   publicly_accessible = false
 
-  multi_az               = true
-  db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.security_group.security_group_id]
+#   multi_az               = true
+#   db_subnet_group_name   = module.vpc.database_subnet_group
+#   vpc_security_group_ids = [module.security_group.security_group_id]
 
-  maintenance_window              = "Mon:00:00-Mon:03:00"
-  backup_window                   = "03:00-06:00"
-  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
-  create_cloudwatch_log_group     = true
+#   maintenance_window              = "Mon:00:00-Mon:03:00"
+#   backup_window                   = "03:00-06:00"
+#   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+#   create_cloudwatch_log_group     = true
 
-  backup_retention_period = 0
-  skip_final_snapshot     = true
-  deletion_protection     = false
+#   backup_retention_period = 0
+#   skip_final_snapshot     = true
+#   deletion_protection     = false
 
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-  create_monitoring_role                = true
-  monitoring_interval                   = 60
-  monitoring_role_name                  = "example-monitoring-role-name"
-  monitoring_role_description           = "Description for monitoring role"
+#   performance_insights_enabled          = true
+#   performance_insights_retention_period = 7
+#   create_monitoring_role                = true
+#   monitoring_interval                   = 60
+#   monitoring_role_name                  = "example-monitoring-role-name"
+#   monitoring_role_description           = "Description for monitoring role"
 
-  parameters = [
-    {
-      name  = "autovacuum"
-      value = 1
-    },
-    {
-      name  = "client_encoding"
-      value = "utf8"
-    }
-  ]
+#   parameters = [
+#     {
+#       name  = "autovacuum"
+#       value = 1
+#     },
+#     {
+#       name  = "client_encoding"
+#       value = "utf8"
+#     }
+#   ]
 
-  tags = local.tags
-  db_option_group_tags = {
-    "Sensitive" = "low"
-  }
-  db_parameter_group_tags = {
-    "Sensitive" = "low"
-  }
-}
+#   tags = local.tags
+#   db_option_group_tags = {
+#     "Sensitive" = "low"
+#   }
+#   db_parameter_group_tags = {
+#     "Sensitive" = "low"
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -11,27 +11,27 @@ output "hcp_vault_private_url" {
   value = hcp_vault_cluster.demo.vault_private_endpoint_url
 }
 
-output "db_instance_endpoint" {
-  description = "The connection endpoint"
-  value       = module.db.db_instance_endpoint
-}
+# output "db_instance_endpoint" {
+#   description = "The connection endpoint"
+#   value       = module.db.db_instance_endpoint
+# }
 
-output "db_instance_name" {
-  description = "The database name"
-  value       = module.db.db_instance_name
-}
+# output "db_instance_name" {
+#   description = "The database name"
+#   value       = module.db.db_instance_name
+# }
 
-output "db_instance_username" {
-  description = "The master username for the database"
-  value       = module.db.db_instance_username
-  sensitive   = true
-}
+# output "db_instance_username" {
+#   description = "The master username for the database"
+#   value       = module.db.db_instance_username
+#   sensitive   = true
+# }
 
-output "db_instance_password" {
-  description = "The database password (this password may be old, because Terraform doesn't track it after initial creation)"
-  value       = module.db.db_instance_password
-  sensitive   = true
-}
+# output "db_instance_password" {
+#   description = "The database password (this password may be old, because Terraform doesn't track it after initial creation)"
+#   value       = module.db.db_instance_password
+#   sensitive   = true
+# }
 
 # # output "db_instance_address" {
 # #   description = "The address of the RDS instance"
